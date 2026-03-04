1efc71873a33ba929faa4a2840b954c9771e26e8
Charlie Gleason <hi@charliegleason.com>
2026-03-04 15:32:27 -0600

    use SharedModelContainer in main app

--- src/ios/Nylon Impossible/Nylon Impossible/Nylon_ImpossibleApp.swift
+++ src/ios/Nylon Impossible/Nylon Impossible/Nylon_ImpossibleApp.swift
@@ -29,7 +29,7 @@ struct Nylon_ImpossibleApp: App {
                     }
                 }
         }
-        .modelContainer(for: TodoItem.self)
+        .modelContainer(SharedModelContainer.shared)
     }
 }
 
